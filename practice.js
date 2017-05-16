6fe3c7a8142e787d4963d205af20f6822120845b
Szilard Magyar <szilard.magyar@gmail.com>
2017-05-16 09:59:26 +0200

    add myMap implementation

--- practice.js
+++ practice.js
@@ -11,3 +11,18 @@ function isEven(n) {
 }
 
 isEven(5);
+
+Array.prototype.myMap = function(callback) {
+  arr = [];
+  for (var i = 0; i < this.length; i++) {
+    arr.push(callback(this[i]));
+  }
+  return arr;
+}
+
+var doubled = [1,2,3].myMap(function(n) {
+  return n ** 2;
+});
+
+console.log(doubled);
+
