b4d59cbc9804cd472d7d87bdcdcf24f5797e5f79
Szilard Magyar <szilard.magyar@gmail.com>
2017-08-29 23:23:29 +0200

    practice

--- practice.js
+++ practice.js
@@ -26,3 +26,24 @@ var doubled = [1,2,3].myMap(function(n) {
 
 console.log(doubled);
 
+
+function rot13(str) { // LBH QVQ VG!
+  
+  var splitStr = str.split('');
+
+  for (var i = 0; i < splitStr.length; i++) {
+    if (splitStr[i] !== ' ') {
+      splitStr[i] = splitStr[i].charCodeAt() + 13;
+      if (splitStr[i] > 90) {
+        splitStr[i] -= 26;
+      }
+      splitStr[i] = String.fromCharCode(splitStr[i]);
+    }
+  }
+  
+  return splitStr.join('');
+}
+
+// Change the inputs below to test
+console.log(rot13("SERR PBQR PNZC"));
+
